efdff2be1f36e29a275b96163da2883d82ab52c1
SanJiang <changsanjiang@gmail.com>
2018-02-26 16:07:37 +0800

    Trim

--- SJBaseVideoPlayer/SJBaseVideoPlayer.m
+++ SJBaseVideoPlayer/SJBaseVideoPlayer.m
@@ -105,9 +105,10 @@ NS_ASSUME_NONNULL_END
 }
 
 - (void)dealloc {
-#ifndef DEBUG
+#ifdef DEBUG
     NSLog(@"%zd - %s", __LINE__, __func__);
 #endif
+    [_presentView removeFromSuperview];
     [self stop];
 }
 
@@ -1037,7 +1038,6 @@ NS_ASSUME_NONNULL_END
     dispatch_async(dispatch_get_main_queue(), ^{
         self.orentationObserver.rotateOrientation = rotateOrientation;
     });
-
 }
 
 - (SJRotateViewOrientation)rotateOrientation {
