c2a826595af8ffeafdf85f414f624c4496622881
changsanjiang <changsanjiang@gmail.com>
2018-06-01 13:06:49 +0800

    加了两个方法
    - 向外提供了查询当前缓冲进度的属性, 现在不仅可以在代理中获取缓冲进度, 还可以直接通过播放器获取
    - 添加了一个可以重置控制层显示状态的方法
    - 将协议分类

--- SJBaseVideoPlayer/Header/SJVideoPlayerControlLayerProtocol.h
+++ SJBaseVideoPlayer/Header/SJVideoPlayerControlLayerProtocol.h
@@ -11,7 +11,7 @@
 #import <UIKit/UIKit.h>
 #import "SJNetworkStatus.h"
 @protocol SJPlayStatusControlDelegate, SJLoadingControlDelegate, SJNetworkStatusControlDelegate, SJLockScreenStateControlDelegate, SJAppActivityControlDelegate, SJVolumeBrightnessRateControlDelegate, SJGestureControlDelegate, SJRotationControlDelegate;
-@class SJBaseVideoPlayer;
+@class SJBaseVideoPlayer, SJVideoPlayerURLAsset;
 
 
 
