fb4ede81b3d440c555cbf6a1de075bd53133d4f3
SanJiang <changsanjiang@gmail.com>
2018-12-11 23:33:54 +0800

    Added `refreshAppearState` into `playModelObserver`

--- SJBaseVideoPlayer/SJBaseVideoPlayer.m
+++ SJBaseVideoPlayer/SJBaseVideoPlayer.m
@@ -495,6 +495,7 @@ static NSString *_kGestureState = @"state";
         dispatch_async(dispatch_get_main_queue(), ^{
             __strong typeof(_self) self = _self;
             if ( !self ) return ;
+            [self.playModelObserver refreshAppearState];
             [self _observeFullscreenPopGestureState];
         });
     }];
