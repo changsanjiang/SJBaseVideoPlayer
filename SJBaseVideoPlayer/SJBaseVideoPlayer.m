66a109ee141eb790722313d801aeba1290b056d5
畅三江 <changsanjiang@gmail.com>
2019-11-04 16:21:28 +0800

    Release 3.0.7
    - 修复 充满全屏后, 播放别的视频可能无法触发旋转的问题
    - 现在 可以设置最小缓冲时间了
    - 修复 播放下一个视频时, 资源状态反馈可能不及时的问题

--- SJBaseVideoPlayer/SJBaseVideoPlayer.m
+++ SJBaseVideoPlayer/SJBaseVideoPlayer.m
@@ -174,7 +174,7 @@ _lookupResponder(UIView *view, Class cls) {
 }
 
 + (NSString *)version {
-    return @"v3.0.6";
+    return @"v3.0.7";
 }
 
 - (void)setVideoGravity:(SJVideoGravity)videoGravity {
