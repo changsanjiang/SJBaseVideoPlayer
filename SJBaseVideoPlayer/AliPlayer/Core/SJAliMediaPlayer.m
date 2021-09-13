c2aebe53ec814b42f3021e3e549e17e61c3f5ca5
Lu Wenlong <1964990256@qq.com>
2021-09-13 14:38:43 +0800

    Update SJAliMediaPlayer.m
    Fix 补全阿里播放器的 WeakProxy

--- SJBaseVideoPlayer/AliPlayer/Core/SJAliMediaPlayer.m
+++ SJBaseVideoPlayer/AliPlayer/Core/SJAliMediaPlayer.m
@@ -25,12 +25,61 @@ NSErrorDomain const SJAliMediaPlayerErrorDomain = @"SJAliMediaPlayerErrorDomain"
     return proxy;
 }
 
-- (nullable NSMethodSignature *)methodSignatureForSelector:(SEL)sel {
-    return [_target methodSignatureForSelector:sel];
+- (id)forwardingTargetForSelector:(SEL)selector {
+    return _target;
 }
 
 - (void)forwardInvocation:(NSInvocation *)invocation {
-    [invocation invokeWithTarget:_target];
+    void *null = NULL;
+    [invocation setReturnValue:&null];
+}
+
+- (nullable NSMethodSignature *)methodSignatureForSelector:(SEL)selector {
+    return [NSObject instanceMethodSignatureForSelector:@selector(init)];
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector {
+    return [_target respondsToSelector:aSelector];
+}
+
+- (BOOL)isEqual:(id)object {
+    return [_target isEqual:object];
+}
+
+- (NSUInteger)hash {
+    return [_target hash];
+}
+
+- (Class)superclass {
+    return [_target superclass];
+}
+
+- (Class)class {
+    return [_target class];
+}
+
+- (BOOL)isKindOfClass:(Class)aClass {
+    return [_target isKindOfClass:aClass];
+}
+
+- (BOOL)isMemberOfClass:(Class)aClass {
+    return [_target isMemberOfClass:aClass];
+}
+
+- (BOOL)conformsToProtocol:(Protocol *)aProtocol {
+    return [_target conformsToProtocol:aProtocol];
+}
+
+- (BOOL)isProxy {
+    return YES;
+}
+
+- (NSString *)description {
+    return [_target description];
+}
+
+- (NSString *)debugDescription {
+    return [_target debugDescription];
 }
 @end
 
